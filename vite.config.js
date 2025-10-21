5e47c1565e546cfbf39b4c51b863b43e7bf5b8cf
LaijieJi <ji.laijie@gmail.com>
2025-10-21 09:58:31 +0200

    fix: adding directory for github action

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Markdown from 'unplugin-vue-markdown/vite';
 
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1];
+
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? `/${repo}/` : '/',
   plugins: [
     vue({
       include: [/\.vue$/, /\.md$/]
